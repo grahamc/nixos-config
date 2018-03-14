57a1186eb611cea5ecc9d03c200492c46f095fc6
Graham Christensen <graham@grahamc.com>
2018-03-14 09:29:26 -0400

    PulseAudio: Bluetooth audio works

--- configuration.nix
+++ configuration.nix
@@ -44,10 +44,23 @@ in {
   '';
 
   hardware = {
-    pulseaudio.enable = true;
+    pulseaudio = {
+      enable = true;
+      package = pkgs.pulseaudioFull;
+
+      #extraConfig = ''
+      #  load-module module-remap-sink master=alsa_output.pci-0000_00_1f.3.analog-stereo sink_name=mono sink_properties="device.description='Mono'" channels=2 channel_map=mono,mono
+      #'';
+    };
     mcelog.enable = true;
-    bumblebee.enable = true;
-    bluetooth.enable = true;
+    # bumblebee.enable = true;
+    bluetooth = {
+      enable = true;
+      extraConfig = ''
+        [general]
+        Enable=Source,Sink,Media,Socket
+      '';
+    };
   };
 
   i18n = {
