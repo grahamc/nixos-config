acb7a24b3f4233282b3b023f59dd7c7ce79bc54e
Graham Christensen <graham@grahamc.com>
2018-07-03 11:24:18 -0400

    Fix passff

--- packages/passff-host/default.nix
+++ packages/passff-host/default.nix
@@ -1,15 +1,19 @@
 { stdenv, fetchFromGitHub, python3, pass }:
 stdenv.mkDerivation {
-  name = "passff-host-1.0.3linux";
+  name = "passff-host-1.3";
 
   src = fetchFromGitHub {
     owner = "passff";
-    repo = "passff";
-    rev = "1.0.3linux";
-    sha256 = "0p1yhqy6aj2qvcd1278irm7bapjpd0ij57w3xz5fq7z2560x0p2j";
+    repo = "passff-host";
+    rev = "1.0.1";
+    sha256 = "1rka47v0apjrj2h0d9vqp7lzjkh75chjk0fpl3bpa67d2197vsah";
   };
 
   buildInputs = [ python3 ];
+
+  patches = [
+    ./paths.patch
+  ];
 
   buildPhase = ''
     patchShebangs .
@@ -17,13 +21,17 @@ stdenv.mkDerivation {
 
   installPhase = ''
     mkdir -p $out/bin
-    cp ./src/host/passff.py $out/bin/passff.py
+    cp ./src/passff.py $out/bin/passff.py
 
     mkdir -p $out/share/passff-host
-    cp ./src/host/passff.json $out/share/passff-host/passff.json
+    cp ./src/passff.json $out/share/passff-host/passff.json
 
     substituteInPlace $out/bin/passff.py \
-      --replace "[receivedMessage['command']]" "['${pass}/bin/pass']"
+      --replace "@PATH@" ""
+    substituteInPlace $out/bin/passff.py \
+      --replace "@PASS@" "${pass}/bin/pass"
+    substituteInPlace $out/bin/passff.py \
+      --replace "@VERSION@" "1.0.1"
 
 
     substituteInPlace $out/share/passff-host/passff.json \
