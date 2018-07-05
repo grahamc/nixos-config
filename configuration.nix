583e47e093834ed6a9f7f17ac0e017910693028d
Graham Christensen <graham@grahamc.com>
2018-07-05 08:44:34 -0400

    Pass --exit-code for somer eason

--- configuration.nix
+++ configuration.nix
@@ -7,9 +7,10 @@ let
     { buildInputs = [ pkgs.git ]; }
     ''
       cd ${srcTree}
-      if ! git diff -q; then
+      if ! git diff --quiet --exit-code; then
         echo "FAIL FAIL FAIL";
         echo "MUST COMMIT YOUR CHANGES! DIRTY TREE DETECTED!"
+        exit 1
       fi
 
       touch $out
