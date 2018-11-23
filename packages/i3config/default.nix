b35f7798dbb5e2e234d1d21cb0f16aa60ce1f361
Graham Christensen <graham@grahamc.com>
2018-11-23 15:31:27 -0500

    fixup

--- packages/i3config/default.nix
+++ packages/i3config/default.nix
@@ -4,7 +4,7 @@ mutate ./config {
   inherit sakura i3status dropbox dmenu pulseaudioFull volume
   backlight dunst dunst_config screenshot;
   i3status_conf = let
-      location = (import /etc/nixos/secrets.nix).location;
+      location = (import ../../secrets-notsecret.nix).location;
     in mutate ./i3status {
       remote_tzs = lib.lists.imap0 (i: tz: ''
         tztime remote${toString i} {
