ad39f2f04ec617a0f6f033a4ebb239931830efb1
Graham Christensen <graham@grahamc.com>
2018-11-23 15:20:12 -0500

    Restore commit

--- pure.nix
+++ pure.nix
@@ -1,7 +1,7 @@
 let
   nixpkgs = builtins.fetchGit {
     url = "https://github.com/nixos/nixpkgs.git";
-    #rev = "b37872d4268164614e3ecef6e1f730d48cf5a90f";
+    rev = "b37872d4268164614e3ecef6e1f730d48cf5a90f";
     ref = "master";
   };
 
