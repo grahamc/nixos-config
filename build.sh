bcbdec41b32caba28c42bb9a69aae081a3794501
Graham Christensen <graham@grahamc.com>
2018-12-08 21:12:56 -0500

    Update build  script

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-nix-build --pure-eval -E 'let r = (builtins.fetchGit { rev = "'$(git rev-parse HEAD)'"; url = ./.; }); in import "${r}/pure.nix"' -A system --show-trace
+nix-build \
+    --pure-eval \
+    -E 'let r = (builtins.fetchGit { rev = "'$(git rev-parse HEAD)'"; url = ./.; }); in import "${r}/pure.nix"' \
+    -A system \
+    --show-trace \
+    -vvvv
