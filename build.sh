750d64d21734541114d6d6db997a81a5c65c0e5d
Graham Christensen <graham@grahamc.com>
2018-12-18 11:50:52 -0500

    Delete -pure stuff

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-nix-build \
-    --pure-eval \
-    -E 'let r = (builtins.fetchGit { rev = "'$(git rev-parse HEAD)'"; url = ./.; }); in import "${r}/pure.nix"' \
-    -A system \
-    --show-trace \
-    -vvvv
