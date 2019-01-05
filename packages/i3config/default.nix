1bbe6c8a8ddaa175e71fb081b76b95f1ac1b7108
Graham Christensen <graham@grahamc.com>
2019-01-04 23:10:58 -0500

    Drop dropbox

--- packages/i3config/default.nix
+++ packages/i3config/default.nix
@@ -1,10 +1,10 @@
-{ mutate, sakura, xorg, i3status, dropbox, dmenu, pulseaudioFull,
+{ mutate, sakura, xorg, i3status, dmenu, pulseaudioFull,
   volume, backlight, dunst, dunst_config, lib, screenshot, secrets }:
 mutate ./config {
-  inherit sakura i3status dropbox dmenu pulseaudioFull volume
+  inherit sakura i3status dmenu pulseaudioFull volume
   backlight dunst dunst_config screenshot;
   i3status_conf = mutate ./i3status {
-      remote_tzs = lib.lists.imap0 (i: tz: ''
+    remote_tzs = lib.lists.imap0 (i: tz: ''
         tztime remote${toString i} {
           format = "-%d %H:%M:%S %Z"
           timezone = "${tz}"
@@ -12,5 +12,5 @@ mutate ./config {
         order += "tztime remote${toString i}"
 
       '') secrets.location.remote_timezones;
-    };
+  };
 }
