19fb2a359e23ea87703623be695a37c3f8699380
tilpner <till@hoeppner.ws>
2019-02-07 16:30:19 +0100

    symlinks: fix for removal of mkOptions.options argument

--- packages/symlinks/service.nix
+++ packages/symlinks/service.nix
@@ -3,34 +3,33 @@
 with lib;
 
 let
-usersWithKeys = attrValues (flip filterAttrs config.users.users (n: u:
+  usersWithKeys = attrValues (flip filterAttrs config.users.users (n: u:
     u.symlinks != {}
-));
+  ));
 
-activationScriptForUser = user: flip mapAttrsToList user.symlinks (reltarget: src:
-  "update_symlink \"${src}\" \"${user.home}/${reltarget}\""
-);
+  activationScriptForUser = user: flip mapAttrsToList user.symlinks (reltarget: src:
+    "update_symlink \"${src}\" \"${user.home}/${reltarget}\""
+  );
 
-activationScript = ''
+  activationScript = ''
     ${builtins.readFile ./update-symlinks.sh}
     ${concatStringsSep "\n" (flatten (map activationScriptForUser usersWithKeys))}
-'';
-
+  '';
 in {
   ###### interface
 
   options = {
 
     users.users = mkOption {
-      options = [{
-        symlinks = mkOption {
+      type = with types; loaOf (submodule {
+        options.symlinks = mkOption {
           default = {};
           description = ''
             An attrset of relative paths and targets to symlink in to
             the user's HOME.
           '';
         };
-      }];
+      });
     };
   };
 
