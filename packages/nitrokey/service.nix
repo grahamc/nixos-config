6b70f51c026077a6a8032003968a685a44cacdd7
Graham Christensen <graham@grahamc.com>
2019-02-19 20:54:18 -0500

    Nitrokey: changed hash

--- packages/nitrokey/service.nix
+++ packages/nitrokey/service.nix
@@ -2,7 +2,7 @@
 {
   services.udev.extraRules = builtins.readFile (pkgs.fetchurl {
     url = "https://www.nitrokey.com/sites/default/files/40-nitrokey.rules";
-    sha256 = "127nghkfd4dl5mkf5xl1mij2ylxhkgg08nlh912xwrrjyjv4y9sa";
+    sha256 = "1208956k294hfc0jfb6p1ypascdylsyizbc7051b5ixj0w3d3shg";
   });
 
   services.pcscd = {
