6747e2f2f50224832ceca8b88f931335edafc490
Graham Christensen <graham@grahamc.com>
2019-03-05 08:16:03 -0500

    Upgrade to ZFS UNstable for nixos 19.03

--- devices/morbo/configuration.nix
+++ devices/morbo/configuration.nix
@@ -24,6 +24,7 @@ in {
     ];
 
   boot = {
+    zfs.enableUnstable = true;
     kernelParams = [ "acpi_rev_override=5" ]; # "acpi_rev_override=1" "pcie_port_pm=off"
     kernel.sysctl = {
       "net.ipv6.conf.all.use_tempaddr" = 2;
