ae5c9007a21b8747a34a87d44bac24686d47e043
Graham Christensen <graham@grahamc.com>
2019-03-05 08:22:05 -0500

    Retain nitrokey source

--- packages/nitrokey/service.nix
+++ packages/nitrokey/service.nix
@@ -1,9 +1,14 @@
 { pkgs, ... }:
-{
-  services.udev.extraRules = builtins.readFile (pkgs.fetchurl {
+let
+  src = pkgs.fetchurl {
     url = "https://www.nitrokey.com/sites/default/files/40-nitrokey.rules";
     sha256 = "1208956k294hfc0jfb6p1ypascdylsyizbc7051b5ixj0w3d3shg";
-  });
+  };
+in {
+  services.udev.extraRules = ''
+    # ${src}
+    ${builtins.readFile src}
+  '';
 
   services.pcscd = {
     enable = true;
