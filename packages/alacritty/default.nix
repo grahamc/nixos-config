c81ed223620853567a4ca6893ae578ec4366aa9f
Graham Christensen <graham@grahamc.com>
2019-05-06 14:36:57 -0400

    Duplicate alacritty from nixos-unstable for some patches

---
+++ packages/alacritty/default.nix
@@ -0,0 +1,123 @@
+{ stdenv,
+  lib,
+  fetchFromGitHub,
+  rustPlatform,
+  cmake,
+  makeWrapper,
+  ncurses,
+  expat,
+  pkgconfig,
+  freetype,
+  fontconfig,
+  libX11,
+  gzip,
+  libXcursor,
+  libXxf86vm,
+  libXi,
+  libXrandr,
+  libGL,
+  python3,
+  xclip,
+  xorg,
+  wayland,
+  libxkbcommon,
+  # Darwin Frameworks
+  cf-private,
+  AppKit,
+  CoreFoundation,
+  CoreGraphics,
+  CoreServices,
+  CoreText,
+  Foundation,
+  OpenGL }:
+
+with rustPlatform;
+
+let
+  rpathLibs = [
+    expat
+    freetype
+    fontconfig
+    libX11
+    libXcursor
+    libXxf86vm
+    libXrandr
+    libGL
+    libXi
+  ] ++ lib.optionals stdenv.isLinux [
+    wayland
+    libxkbcommon
+    xorg.libxcb
+  ];
+in buildRustPackage rec {
+  pname = "alacritty";
+  version = "0.3.0";
+
+  src = fetchFromGitHub {
+    owner = "jwilm";
+    repo = pname;
+    rev = "f3cc7c4c4e46778eedab18c4d25c7009b9bbb04f";
+    sha256 = "sha256:15vmkjv0lkjp68wphaijar5zrvblg82z17yg66dr374xxszsn5kf";
+  };
+
+  cargoSha256 = "sha256:18vlavvkrwlhg715gw675rlvqzb4s2v7h6d5h4jzv2ab18g9x4n9";
+
+  nativeBuildInputs = [
+    cmake
+    makeWrapper
+    pkgconfig
+    ncurses
+    gzip
+  ];
+
+  buildInputs = rpathLibs
+    ++ [ python3 ]
+    ++ lib.optionals stdenv.isDarwin [
+      AppKit CoreFoundation CoreGraphics CoreServices CoreText Foundation OpenGL
+      # Needed for CFURLResourceIsReachable symbols.
+      cf-private
+    ];
+
+  outputs = [ "out" "terminfo" ];
+
+  postBuild = lib.optionalString stdenv.isDarwin "make app";
+
+  installPhase = ''
+    runHook preInstall
+
+    install -D target/release/alacritty $out/bin/alacritty
+
+  '' + (if stdenv.isDarwin then ''
+    mkdir $out/Applications
+    cp -r target/release/osx/Alacritty.app $out/Applications/Alacritty.app
+  '' else ''
+    install -D extra/linux/alacritty.desktop -t $out/share/applications/
+    install -D extra/logo/alacritty-term.svg $out/share/icons/hicolor/scalable/apps/Alacritty.svg
+    patchelf --set-rpath "${stdenv.lib.makeLibraryPath rpathLibs}" $out/bin/alacritty
+  '') + ''
+
+    install -D extra/completions/_alacritty -t "$out/share/zsh/site-functions/"
+    install -D extra/completions/alacritty.bash -t "$out/etc/bash_completion.d/"
+    install -D extra/completions/alacritty.fish -t "$out/share/fish/vendor_completions.d/"
+
+    install -dm 755 "$out/share/man/man1"
+    gzip -c extra/alacritty.man > "$out/share/man/man1/alacritty.1.gz"
+
+    install -dm 755 "$terminfo/share/terminfo/a/"
+    tic -x -o "$terminfo/share/terminfo" extra/alacritty.info
+    mkdir -p $out/nix-support
+    echo "$terminfo" >> $out/nix-support/propagated-user-env-packages
+
+    runHook postInstall
+  '';
+
+  dontPatchELF = true;
+
+  meta = with stdenv.lib; {
+    description = "GPU-accelerated terminal emulator";
+    homepage = https://github.com/jwilm/alacritty;
+    license = with licenses; [ asl20 ];
+    maintainers = with maintainers; [ mic92 ];
+    platforms = [ "x86_64-linux" "x86_64-darwin" ];
+  };
+}
