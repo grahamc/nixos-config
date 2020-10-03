7846b6a7ec09b784675843f52fdc7088e7b800f0
Graham Christensen <graham@grahamc.com>
2020-10-03 13:02:07 -0400

    add a hint to how to use snoop's logs

--- packages/snoop/default.nix
+++ packages/snoop/default.nix
@@ -1,4 +1,6 @@
 { resholve, mutate, vim, oil, gzip, coreutils, pstree, jq, utillinux }:
+# decode the logs from this via:
+# journalctl -t snoopy -o json --output-fields _PID,MESSAGE | jq -rs '. | group_by(._PID) | .[] | [ .[] | .MESSAGE ] | join("") | @base64d'
 target:
 resholve {
   src = mutate ./snoop.sh {
