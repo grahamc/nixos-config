7a99ec66ef04afb99185d88cf957fba9022997e1
Graham Christensen <graham@grahamc.com>
2020-10-08 08:01:49 -0400

    Run a polkit authentication gui program for my user

--- devices/petunia/configuration.nix
+++ devices/petunia/configuration.nix
@@ -437,6 +437,20 @@ in
     };
   };
 
+  systemd.user.services.polkit-gnome-authentication-agent-1 = {
+    description = "polkit-gnome-authentication-agent-1";
+    wants = [ "graphical-session.target" ];
+    wantedBy = [ "graphical-session.target" ];
+    after = [ "graphical-session.target" ];
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+      Restart = "on-failure";
+      RestartSec = 1;
+      TimeoutStopSec = 10;
+    };
+  };
+
   systemd.user.services.sway = {
     description = "Sway - Wayland window manager";
     documentation = [ "man:sway(5)" ];
